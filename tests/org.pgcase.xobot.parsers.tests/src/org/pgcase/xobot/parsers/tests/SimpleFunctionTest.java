fa05f1cf7e7fe04e44535b2e704dd469f17a430b
Andrew Khitrin <ahitrin@gmail.com>
2019-01-08 16:07:27 +0300

    More tests

--- tests/org.pgcase.xobot.parsers.tests/src/org/pgcase/xobot/parsers/tests/SimpleFunctionTest.java
+++ tests/org.pgcase.xobot.parsers.tests/src/org/pgcase/xobot/parsers/tests/SimpleFunctionTest.java
@@ -1,6 +1,7 @@
 package org.pgcase.xobot.parsers.tests;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -10,7 +11,6 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.pgcase.xobot.parsers.postgres.function.PGFunctionParseHeader;
 import org.pgcase.xobot.parsers.postgres.function.RawFunctionBase;
-import org.pgcase.xobot.parsers.postgres.function.RawFunctionBaseImpl;
 
 class SimpleFunctionTest {
 	
@@ -22,7 +22,43 @@ class SimpleFunctionTest {
 		List<String> errors = new ArrayList<>();
 		RawFunctionBase pg_function_header = PGFunctionParseHeader.parse(in, errors);
 		assertEquals(4, pg_function_header.getArgsCount(), "Incorrect number of argument");
+		//System.out.println(pg_function_header);
+	}
+	
+	@Test
+	void testClasses() throws IOException {
+		FileInputStream in = new FileInputStream(SIMPLE_IN_OUT_DEF_FUNC);
+		List<String> errors = new ArrayList<>();
+		RawFunctionBase pg_function_header = PGFunctionParseHeader.parse(in, errors);
+		assertEquals("IN",pg_function_header.getArg(0).getArgClass(),"Incorrect class of argument 1");
+		assertEquals("IN",pg_function_header.getArg(1).getArgClass(),"Incorrect class of argument 2");
+		assertEquals("IN",pg_function_header.getArg(2).getArgClass(),"Incorrect class of argument 3");
+		assertEquals("OUT",pg_function_header.getArg(3).getArgClass(),"Incorrect class of argument 4");
+		//System.out.println(pg_function_header);
+	}
+
+	@Test
+	void testTypes() throws IOException {
+		FileInputStream in = new FileInputStream(SIMPLE_IN_OUT_DEF_FUNC);
+		List<String> errors = new ArrayList<>();
+		RawFunctionBase pg_function_header = PGFunctionParseHeader.parse(in, errors);
+		assertEquals("integer",pg_function_header.getArg(0).getArgType().toLowerCase(),"Incorrect type of argument 1");
+		assertEquals("integer",pg_function_header.getArg(1).getArgType().toLowerCase(),"Incorrect type of argument 2");
+		assertEquals("integer",pg_function_header.getArg(2).getArgType().toLowerCase(),"Incorrect type of argument 3");
+		assertEquals("integer",pg_function_header.getArg(3).getArgType().toLowerCase(),"Incorrect type of argument 4");
 		System.out.println(pg_function_header);
 	}
 	
+	@Test
+	void testDefaults() throws IOException {
+		FileInputStream in = new FileInputStream(SIMPLE_IN_OUT_DEF_FUNC);
+		List<String> errors = new ArrayList<>();
+		RawFunctionBase pg_function_header = PGFunctionParseHeader.parse(in, errors);
+		assertNull(pg_function_header.getArg(0).getArgDefault(),"Incorrect default value of argument 1");
+		assertEquals("100500",pg_function_header.getArg(1).getArgDefault(),"Incorrect default value of argument 2");
+		assertEquals("-1000",pg_function_header.getArg(2).getArgDefault(),"Incorrect default value of argument 3");
+		assertNull(pg_function_header.getArg(3).getArgDefault(),"Incorrect default value of argument 4");
+		//System.out.println(pg_function_header);
+	}
+
 }

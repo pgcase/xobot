545e13f6e34acdadb38177a1b8969ece8b6c4412
Alexander Fedorov <alexander.fedorov@arsysop.ru>
2019-01-11 11:13:16 +0300

    implement AntlrXTriggerExtractor

--- bundles/org.pgcase.xobot.parsers/src/org/pgcase/xobot/runtime/antlr/AntlrXTriggerExtractor.java
+++ bundles/org.pgcase.xobot.parsers/src/org/pgcase/xobot/runtime/antlr/AntlrXTriggerExtractor.java
@@ -1,5 +1,6 @@
 package org.pgcase.xobot.runtime.antlr;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.Collections;
 import java.util.Map;
@@ -14,7 +15,13 @@ public class AntlrXTriggerExtractor implements XTriggerExtractor {
 	public Iterable<XTriggerDescriptor> extractTriggers(Object input, Map<String, Object> context, XIssueReporter reporter) {
 		if (input instanceof InputStream) {
 			InputStream inputStream = (InputStream) input;
-			
+			try {
+				XTriggerDescriptor parsed = PGTriggerParseHeader.parse(inputStream, reporter);
+				return Collections.singletonList(parsed);
+			} catch (IOException e) {
+				String message = String.format("Failed to process input with context %s", context);
+				reporter.reportIssue(this, input, message, e);
+			}
 		}
 		return Collections.emptyList();
 	}

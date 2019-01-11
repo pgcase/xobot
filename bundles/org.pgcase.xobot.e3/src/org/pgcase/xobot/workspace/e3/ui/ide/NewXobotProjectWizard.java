739ad82caf2fc4ba3ac9fc615bcdb9397b5a4d0c
Alexander Fedorov <alexander.fedorov@arsysop.ru>
2019-01-11 21:49:21 +0300

    add new xobot project wizard

---
+++ bundles/org.pgcase.xobot.e3/src/org/pgcase/xobot/workspace/e3/ui/ide/NewXobotProjectWizard.java
@@ -0,0 +1,12 @@
+package org.pgcase.xobot.workspace.e3.ui.ide;
+
+import org.eclipse.ui.INewWizard;
+import org.eclipse.ui.wizards.newresource.BasicNewProjectResourceWizard;
+
+public class NewXobotProjectWizard extends BasicNewProjectResourceWizard implements INewWizard {
+
+	public NewXobotProjectWizard() {
+		setWindowTitle("New Xobot Project");
+	}
+	
+}

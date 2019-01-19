4108725411a2b10d534a5c37ec3662b83a2aa73f
Alexander Fedorov <alexander.fedorov@arsysop.ru>
2019-01-19 09:03:31 +0300

    add copyrights to .java

--- tests/org.pgcase.xobot.dbproc.antlr.tests/src/org/pgcase/xobot/dbproc/antlr/tests/SimpleFunctionTest.java
+++ tests/org.pgcase.xobot.dbproc.antlr.tests/src/org/pgcase/xobot/dbproc/antlr/tests/SimpleFunctionTest.java
@@ -1,17 +1,35 @@
+/*******************************************************************************
+ * Copyright (c) 2018 ArSysOp
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Contributors:
+ *     ArSysOp - initial API and implementation
+ *******************************************************************************/
 package org.pgcase.xobot.dbproc.antlr.tests;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.pgcase.xobot.dbproc.antlr.functions.PgFunctionParseHeader;
 import org.pgcase.xobot.dbproc.runtime.functions.XFunctionDescriptor;
 
+@SuppressWarnings("nls")
 class SimpleFunctionTest {
 	
 	private static final String SIMPLE_IN_OUT_DEF_FUNC = "sql/simple_empty_function_with_IN_OUT_DEF_arguments.sql"; //$NON-NLS-1$

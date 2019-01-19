ff8f203d248d140579bb3e40af58029399b8f840
Alexander Fedorov <alexander.fedorov@arsysop.ru>
2019-01-19 09:48:58 +0300

    add copyrights to generated sources

--- bundles/org.pgcase.xobot.landscape.model.edit/src-gen/org/pgcase/xobot/landscape/model/edit/provider/SourceSetItemProvider.java
+++ bundles/org.pgcase.xobot.landscape.model.edit/src-gen/org/pgcase/xobot/landscape/model/edit/provider/SourceSetItemProvider.java
@@ -1,5 +1,23 @@
-/**
- */
+/*******************************************************************************
+ * Copyright (c) 2018-2019 ArSysOp
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Contributors:
+ *     ArSysOp - initial API and implementation
+ *******************************************************************************/
 package org.pgcase.xobot.landscape.model.edit.provider;
 
 
@@ -25,6 +43,7 @@ import org.eclipse.emf.edit.provider.ItemPropertyDescriptor;
 import org.eclipse.emf.edit.provider.ItemProviderAdapter;
 import org.eclipse.emf.edit.provider.ViewerNotification;
 import org.pgcase.xobot.landscape.model.api.XSourceSet;
+import org.pgcase.xobot.landscape.model.edit.LandscapeEditPlugin;
 import org.pgcase.xobot.landscape.model.meta.XLandscapeFactory;
 import org.pgcase.xobot.landscape.model.meta.XLandscapePackage;
 
@@ -229,7 +248,7 @@ public class SourceSetItemProvider
 	 */
 	@Override
 	public ResourceLocator getResourceLocator() {
-		return org.pgcase.xobot.landscape.model.edit.LandscapeEditPlugin.INSTANCE;
+		return LandscapeEditPlugin.INSTANCE;
 	}
 
 }

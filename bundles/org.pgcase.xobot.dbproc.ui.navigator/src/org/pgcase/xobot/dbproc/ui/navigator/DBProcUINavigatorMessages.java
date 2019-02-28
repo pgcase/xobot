0400233fbffbf04b7035f353f8f7f8fec1af3c5b
kefir <kefir.fedorov@gmail.com>
2019-02-28 20:24:46 +0300

    i18n and l10n

---
+++ bundles/org.pgcase.xobot.dbproc.ui.navigator/src/org/pgcase/xobot/dbproc/ui/navigator/DBProcUINavigatorMessages.java
@@ -0,0 +1,15 @@
+package org.pgcase.xobot.dbproc.ui.navigator;
+
+import org.eclipse.osgi.util.NLS;
+
+public class DBProcUINavigatorMessages extends NLS {
+	private static final String BUNDLE_NAME = "org.pgcase.xobot.dbproc.ui.navigator.DBProcUINavigatorMessages"; //$NON-NLS-1$
+	public static String FunctionsActionProvider_action_open_title;
+	static {
+		// initialize resource bundle
+		NLS.initializeMessages(BUNDLE_NAME, DBProcUINavigatorMessages.class);
+	}
+
+	private DBProcUINavigatorMessages() {
+	}
+}

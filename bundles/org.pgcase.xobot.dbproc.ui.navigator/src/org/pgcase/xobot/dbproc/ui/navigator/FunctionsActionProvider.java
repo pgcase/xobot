c7c3790c3b634631425be92a08387a7eb0f09e7b
Alexander Fedorov <alexander.fedorov@arsysop.ru>
2020-02-01 14:15:27 +0300

    Update headers
    Xobot DB Proc
    
    Signed-off-by: Alexander Fedorov <alexander.fedorov@arsysop.ru>

--- bundles/org.pgcase.xobot.dbproc.ui.navigator/src/org/pgcase/xobot/dbproc/ui/navigator/FunctionsActionProvider.java
+++ bundles/org.pgcase.xobot.dbproc.ui.navigator/src/org/pgcase/xobot/dbproc/ui/navigator/FunctionsActionProvider.java
@@ -11,7 +11,7 @@ import org.eclipse.ui.navigator.ICommonActionExtensionSite;
 import org.pgcase.xobot.basis.ui.navigator.RegistryActionProvider;
 
 /*******************************************************************************
- * Copyright (c) 2018-2019 ArSysOp
+ * Copyright (c) 2019-2020 ArSysOp
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
